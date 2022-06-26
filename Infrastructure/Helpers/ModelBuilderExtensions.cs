2570cd164a5810b15d286efd90ee95d4c1f9b01e
Pablo B. Martinez Pedroso <pmartinezpedroso@gmail.com>
2022-06-25 22:36:22 -0400

    Initial Migration and seeded data
    1. Add initial migration
    2. Seed initial data (Users and Projects)

---
+++ Infrastructure/Helpers/ModelBuilderExtensions.cs
@@ -0,0 +1,32 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure.Helpers
+{
+    public static class ModelBuilderExtensions
+    {
+        public static void Seed(this ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().HasData
+                (
+                    new User() { Id = 1, Name = "James", SurName = "Allen" },
+                    new User() { Id = 2, Name = "Grant", SurName = "Slaughter" },
+                    new User() { Id = 3, Name = "Ronald ", SurName = "Brown" },
+                    new User() { Id = 4, Name = "Aaron", SurName = "Lawrence" },
+                    new User() { Id = 5, Name = "Randy", SurName = "Greenwood" }
+                );
+
+            modelBuilder.Entity<Project>().HasData
+                (
+                    new Project() { Id = 1, Name = "Sharpen Design", Description = "Has generated over five million design prompts to help you prep for an interview." },
+                    new Project() { Id = 2, Name = "Briefz", Description = "For creating a simple and fun brief generator for every kind of designer." },
+                    new Project() { Id = 3, Name = "Designercize", Description = "Digital version of an analog whiteboard exercise meant to test your skills as a designer." },
+                    new Project() { Id = 4, Name = "The Daily Logo Challenge", Description = "Expect to receive a daily email containing a new logo design challenge to tackle." },
+                    new Project() { Id = 5, Name = "Daily UI", Description = " Dribbblers are constantly posting and sharing their work from this challenge." }
+                );
+        }
+    }
+}
---
+++ Infrastructure/Migrations/20220626022714_Init.cs
@@ -0,0 +1,113 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Infrastructure.Migrations
+{
+    public partial class Init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Projects",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: false),
+                    Description = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Projects", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: false),
+                    SurName = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Bugs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Description = table.Column<string>(maxLength: 100, nullable: false),
+                    UserId = table.Column<int>(nullable: false),
+                    ProjectId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Bugs", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Bugs_Projects_ProjectId",
+                        column: x => x.ProjectId,
+                        principalTable: "Projects",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Bugs_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Projects",
+                columns: new[] { "Id", "Description", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Has generated over five million design prompts to help you prep for an interview.", "Sharpen Design" },
+                    { 2, "For creating a simple and fun brief generator for every kind of designer.", "Briefz" },
+                    { 3, "Digital version of an analog whiteboard exercise meant to test your skills as a designer.", "Designercize" },
+                    { 4, "Expect to receive a daily email containing a new logo design challenge to tackle.", "The Daily Logo Challenge" },
+                    { 5, " Dribbblers are constantly posting and sharing their work from this challenge.", "Daily UI" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "Name", "SurName" },
+                values: new object[,]
+                {
+                    { 1, "James", "Allen" },
+                    { 2, "Grant", "Slaughter" },
+                    { 3, "Ronald ", "Brown" },
+                    { 4, "Aaron", "Lawrence" },
+                    { 5, "Randy", "Greenwood" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Bugs_ProjectId",
+                table: "Bugs",
+                column: "ProjectId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Bugs_UserId",
+                table: "Bugs",
+                column: "UserId",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Bugs");
+
+            migrationBuilder.DropTable(
+                name: "Projects");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
